3429422ae74598ab44563fbcd9449b122d895641
viniciusfurtado1 <vinicius_furtado10@hotmail.com>
2024-08-14 21:09:14 -0300

    refatoracao

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:idate_libras/idate_e/idate_e_intrucoes.dart';
-//import 'package:idate_libras/idate_instrucoes.dart';
 import 'package:idate_libras/idate_t/idate_t_intrucoes.dart';
 import 'package:idate_libras/result_page.dart';
 
@@ -19,16 +18,6 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
-        /*leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(builder: (context) => const IdateInstrucoes()),
-              (Route<dynamic> route) => false,
-            );
-          },
-        ),*/
         title: const Text(
           "IDATE/Libras",
           style: TextStyle(fontWeight: FontWeight.normal, color: Colors.white),
@@ -54,12 +43,6 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                /*const Text(
-                  "INSTRUÇÕES INICIAIS",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Image.asset('assets/images/video.png'),
-                */
                 const SizedBox(height: 0.0),
                 const Text(
                   "SELECIONE O FORMULÁRIO:",
@@ -110,27 +93,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-                /*Row(
-                  children: [
-                    Transform.scale(
-                      scale: 1.5,
-                      child: Radio<SingingCharacter>(
-                        value: SingingCharacter.idatete,
-                        groupValue: _character,
-                        onChanged: (SingingCharacter? value) {
-                          setState(() {
-                            _character = value;
-                          });
-                        },
-                      ),
-                    ),
-                    const SizedBox(width: 16.0),
-                    const Text(
-                      'IDATE-T e IDATE-E',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),*/
               ],
             ),
           ),
@@ -144,10 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           } else if (_character == SingingCharacter.idatee) {
             Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => const IdateEInstrucoes()));
-          } /*else if (_character == SingingCharacter.idatete) {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => IdateTeE()));
-          }*/
+          } 
         },
         shape: const CircleBorder(),
         backgroundColor: Theme.of(context).colorScheme.primary,
